5fd34da942e63729a5da33dbad592332d6e00592
GianniZheng <152515879+GianniZheng@users.noreply.github.com>
2024-10-10 21:11:20 +0200

    ex complete

--- Lezione 24/index.js
+++ Lezione 24/index.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static("html"));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/html/home.html");
+})
+
+app.get('/services', (req, res) => {
+  res.sendFile(__dirname + "/html/services.html");
+})
+
+app.get('/contacts', (req, res) => {
+  res.sendFile(__dirname + "/html/contacts.html");
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + "/html/about.html");
 })
 
 
